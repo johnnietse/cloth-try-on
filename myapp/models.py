66239aecf30aa0ce9c1b573ec66abb373f4ad3ce
Johnnie Tse <johnnietse994@gmail.com>
2025-05-12 20:53:41 -0400

    commit on May 12th 8:53pm

---
+++ myapp/models.py
@@ -0,0 +1,24 @@
+# from .extensions import db
+#
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(50))
+#
+#
+
+
+from myapp.extensions import db
+from datetime import datetime
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+
+
+class VideoUpload(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_filename = db.Column(db.String(100))
+    processed_filename = db.Column(db.String(100))
+    shirt_index = db.Column(db.Integer)
+    upload_date = db.Column(db.DateTime, default=datetime.utcnow)
