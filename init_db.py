1f266ba48cf2212aea8ee6984d7e13d88dbc450c
Johnnie Tse <johnnietse994@gmail.com>
2025-05-13 00:21:03 -0400

    commit on May 13th 12:20am

--- init_db.py
+++
@@ -1,7 +0,0 @@
-from myapp import create_app
-from myapp.extensions import db
-
-app = create_app()
-with app.app_context():
-    db.create_all()
-    print("Database tables initialized")
--- run.py
+++ run.py
@@ -1,13 +1,3 @@
 # from myapp import create_app
 #
 # app = create_app()
-
-
-from myapp import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run()
-
-
