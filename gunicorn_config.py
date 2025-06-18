c942691ceb056843b830513a41222d039f3c413d
johnnietse <johnnietse994@gmail.com>
2025-06-18 12:27:00 -0400

    fix app.py, render.yaml, and gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,23 +1,31 @@
+# import multiprocessing
+#
+# # Server socket
+# bind = "0.0.0.0:5000"
+#
+# # Worker processes
+# workers = multiprocessing.cpu_count() * 2 + 1
+# threads = 2
+# worker_class = "gthread"
+#
+# # Timeout
+# timeout = 120
+# keepalive = 5
+#
+# # Logging
+# accesslog = "-"
+# errorlog = "-"
+# loglevel = "info"
+#
+# # Security
+# limit_request_line = 4094
+# limit_request_fields = 100
+# limit_request_field_size = 8190
+
+
 import multiprocessing
 
-# Server socket
-bind = "0.0.0.0:5000"
-
-# Worker processes
-workers = multiprocessing.cpu_count() * 2 + 1
-threads = 2
-worker_class = "gthread"
-
-# Timeout
-timeout = 120
-keepalive = 5
-
-# Logging
-accesslog = "-"
-errorlog = "-"
-loglevel = "info"
-
-# Security
-limit_request_line = 4094
-limit_request_fields = 100
-limit_request_field_size = 8190
+workers = 1  # or 2 for free tier safety
+timeout = 360  # allow up to 2 minutes
+worker_class = 'sync'
+bind = '0.0.0.0:8000'
