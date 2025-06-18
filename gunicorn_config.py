bac7bd6edd752c0f2c3ecbebcd29987683928287
johnnietse <johnnietse994@gmail.com>
2025-06-18 18:01:45 -0400

    organize the github repo

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,28 +1,3 @@
-# import multiprocessing
-#
-# # Server socket
-# bind = "0.0.0.0:5000"
-#
-# # Worker processes
-# workers = multiprocessing.cpu_count() * 2 + 1
-# threads = 2
-# worker_class = "gthread"
-#
-# # Timeout
-# timeout = 120
-# keepalive = 5
-#
-# # Logging
-# accesslog = "-"
-# errorlog = "-"
-# loglevel = "info"
-#
-# # Security
-# limit_request_line = 4094
-# limit_request_fields = 100
-# limit_request_field_size = 8190
-
-
 import multiprocessing
 
 workers = 1  # or 2 for free tier safety
