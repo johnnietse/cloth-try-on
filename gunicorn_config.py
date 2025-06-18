4090824d5153a4004e87eec388a266e0363672dc
johnnietse <johnnietse994@gmail.com>
2025-06-18 13:08:11 -0400

    fix app.py, render.yaml, and gunicorn_config.py.

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -26,6 +26,6 @@
 import multiprocessing
 
 workers = 1  # or 2 for free tier safety
-timeout = 360  # allow up to 2 minutes
+timeout = 120  # allow up to 2 minutes
 worker_class = 'sync'
 bind = '0.0.0.0:8000'
