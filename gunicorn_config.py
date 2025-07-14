598569ea2d1fd6d29e6dc2c7887d0b5ab5bb8189
Johnnie Tse <138593553+johnnietse@users.noreply.github.com>
2025-07-14 17:25:48 -0400

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,8 +1,14 @@
 import multiprocessing
 
-workers = 1  # or 2 for free tier safety
-timeout = 120  # allow up to 2 minutes
-worker_class = 'sync'
-bind = '0.0.0.0:8000'
+# workers = 1  # or 2 for free tier safety
+# timeout = 120  # allow up to 2 minutes
+# worker_class = 'sync'
+# bind = '0.0.0.0:8000'
 
 
+workers = 4
+worker_class = "sync"
+worker_tmp_dir = "/dev/shm"
+bind = "0.0.0.0:10000"
+timeout = 120
+keepalive = 5
